6b66704662c485a77983afd91b9bc65d0d21417a
Luan Teixeira <luan.teixeira@izatta.tech>
2023-11-11 09:32:15 -0300

    ajuste query

--- src/index.js
+++ src/index.js
@@ -5,40 +5,36 @@ async function main() {
 
   for (const environment of environments) {
     for (const service of environment.databases) {
-      if (service.serviceName.includes("autenticacao")) {
-        const url = service.dbUrl.split("//")[1].split(":")[0];
-        const port = service.dbUrl.split("//")[1].split(":")[1].split("/")[0];
-        const database = service.dbUrl
-          .split("//")[1]
-          .split(":")[1]
-          .split("/")[1];
+      if (service.serviceName.includes("rotina")) {
+        try {
+          const url = service.dbUrl.split("//")[1].split(":")[0];
+          const port = service.dbUrl.split("//")[1].split(":")[1].split("/")[0];
+          const database = service.dbUrl
+            .split("//")[1]
+            .split(":")[1]
+            .split("/")[1];
 
-        const query = `
-        DO $$
-        DECLARE
-          qtd INT = 0;
-          begin
-            SELECT count(*) INTO qtd FROM autenticacao.t_usuario WHERE email = 'juliano.santos@izatta.tech';
-            IF qtd <= 0 THEN
-              INSERT INTO autenticacao.t_usuario (nome, email, login, valor_senha, reset_token_senha, tentativas_erro_senha, data_ultima_alteracao_senha, status, id_usuario_atualizacao, data_atualizacao, "version")
-              VALUES('Juliano Santos', 'juliano.santos@izatta.tech', 'juliano.santos', null, 'reset-token$2020-juliano.santos', 0, null, 'PENDENTE'::character varying, 0, null, 0);
-              
-              INSERT INTO autenticacao.t_usuario_grupo (id_usuario, id_grupo) 
-              VALUES ((SELECT id FROM autenticacao.t_usuario WHERE login = 'juliano.santos'), 5);
-          END IF;
-        END $$;
-        `;
+          const query = `
+          CREATE INDEX IF NOT EXISTS t_detalhamento_rotina_id_rotina_idx ON rotina.t_detalhamento_rotina (id_rotina);
 
-        await runQuery(
-          {
-            url: url,
-            port: port,
-            database: database,
-            user: service.dbUser,
-            password: service.dbPassword,
-          },
-          query
-        );
+          CREATE INDEX IF NOT EXISTS t_rotina_tipo_idx ON rotina.t_rotina (tipo,status);
+          `;
+
+          await runQuery(
+            {
+              url: url,
+              port: port,
+              database: database,
+              user: service.dbUser,
+              password: service.dbPassword,
+            },
+            query
+          );
+        } catch (ex) {
+          if (ex instanceof Error) {
+            console.error(ex.message);
+          }
+        }
       }
     }
   }
